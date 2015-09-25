28f44032fc6081fad8a02d4a819dcba70966d0a4
Vaughn Dice <donvice@gmail.com>
2015-09-25 11:13:17 -0700

    add single-question component, new-question route

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,24 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewQuestion: false,
-
   actions: {
-    queryShow() {
-      this.set('addNewQuestion', true);
-    },
-    queryHide() {
-      this.set('addNewQuestion', false);
-    },
-
     saveQuestion() {
       var params = {
         query: this.get('query'),
         notes: this.get('notes'),
         author: this.get('author')
       };
-
-      this.set('addNewQuestion', false),
+      
       this.sendAction('saveQuestion', params);
     }
   }
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('question', {path: '/question/:question_id'});
+  this.route('new-question', {path: '/question/new'});
 });
 
 export default Router;
